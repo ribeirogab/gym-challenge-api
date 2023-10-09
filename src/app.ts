69a855bd95008b58dada2b2caea3c7acaba5a09e
ribeirogab <ribeirogabx@gmail.com>
2023-10-08 23:30:12 -0300

    ci: update ci

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import Fastify from 'fastify';
 
 const app = Fastify();
 
-app.get('/', async (_request, _reply) => {
+app.get('/test', async (_request, _reply) => {
   return { msg: 'Hello! from Fastify!' };
 });
 
-app.post('/', async (request, _reply) => {
+app.post('/test', async (request, _reply) => {
   const body = request.body;
 
   return { msg: 'Fastify received your post request!', body };
